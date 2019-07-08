109cbb566446ea60d0b6ed5d32d61b4b337ae87d
kimchunyong <lovemy5854@naver.com>
2019-07-08 22:50:00 +0900

    [add] antd

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 import { Menu, Input } from 'antd';
 
-const AppLayout = () => {
+const AppLayout = ({ children }) => {
     return (
         <div>
-            <Menu>
-                <Menu.Item>노드버드</Menu.Item>
-                <Menu.Item>노드버드</Menu.Item>
-                <Menu.Item>노드버드</Menu.Item>
+            <Menu mode="horizontal">
+                <Menu.Item key="home">노드버드</Menu.Item>
+                <Menu.Item key="profile">프로필</Menu.Item>
 
-                <Menu.Item>
-                    <Input.Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
+                <Menu.Item key="mail">
+                    <Input.Search placeholder="input search text" style={{ verticalAlign: "middle" }} enterButton />
                 </Menu.Item>
             </Menu>
+            {children}
         </div>
     )
 }
