ec93206a1e61c2e037a90956634bff02661552f1
kimchunyong <lovemy58511@gmail.com>
2019-07-23 22:20:11 +0900

    [add] make form

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 
 import AppLayout from '../components/AppLayout';
@@ -6,17 +6,39 @@ import AppLayout from '../components/AppLayout';
 import { Form, Input, Button, Checkbox } from 'antd';
 
 const Signup = () => {
+    const [id, setId] = useState('');
+    const [nick, setNick] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [term, setTerm] = useState('');
+
+
     const onSubmit = () => { }
 
-    const onChangeId = () => { }
+    const onChangeId = (e) => {
+        setId(getValue(e));
+    }
 
-    const onChangeNick = () => { }
+    const onChangeNick = (e) => {
+        setNick(getValue(e));
+    }
 
-    const onChangePassword = () => { }
+    const onChangePassword = (e) => {
+        setPassword(getValue(e));
+    }
 
-    const onChangePasswordChk = () => { }
+    const onChangePasswordChk = (e) => {
+        setPasswordCheck(getValue(e));
+    }
 
-    const onChangeTerm = () => { }
+    const onChangeTerm = (e) => {
+        setTerm(getValue(e));
+    }
+
+    const getValue = (e) => {
+        const { value } = e.target;
+        return value;
+    }
 
     return (
         <>
@@ -29,28 +51,28 @@ const Signup = () => {
                     <div>
                         <label htmlFor="user-id">아이디</label>
                         <br />
-                        <Input name="user-id" required onChange={onChangeId} />
+                        <Input name="user-id" value={id} required onChange={onChangeId} />
                     </div>
 
                     <div>
                         <label htmlFor="user-nick">닉네임</label>
                         <br />
-                        <Input name="user-nick" required onChange={onChangeNick} />
+                        <Input name="user-nick" value={nick} required onChange={onChangeNick} />
                     </div>
 
                     <div>
-                        <label htmlFor="user-pass">비밀번호</label>
+                        <label htmlFor="user-password">비밀번호</label>
                         <br />
-                        <Input name="user-pass" type="password" required onChange={onChangePassword} />
+                        <Input name="user-pass" value={password} type="password" required onChange={onChangePassword} />
                     </div>
 
                     <div>
-                        <label htmlFor="user-pass-chk">비밀번호확인</label>
+                        <label htmlFor="user-password-check">비밀번호확인</label>
                         <br />
-                        <Input name="user-pass-chk" type="password" required onChange={onChangePasswordChk} />
+                        <Input name="user-password-check" value={passwordCheck} type="password" required onChange={onChangePasswordChk} />
                     </div>
                     <div>
-                        <Checkbox name="user-term" onChange={onChangeTerm}>약관에 동의합니다.</Checkbox>
+                        <Checkbox name="user-term" value={term} onChange={onChangeTerm}>약관에 동의합니다.</Checkbox>
                     </div>
                     <div>
                         <Button type="primary" htmlType="submit">가입하기</Button>
